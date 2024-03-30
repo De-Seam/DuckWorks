4f250c1f65688125f2e9be6d8f89e228f1cfbc77
Sam van der Hoeven <hoevens@yahoo.com>
2024-03-30 22:34:54 +0100

    WIP refobject weakref

--- DuckWorks/DuckWorks/include/Core/Utilities/RefObject.h
+++ DuckWorks/DuckWorks/include/Core/Utilities/RefObject.h
@@ -5,11 +5,19 @@ class RefObject : public RTTIBaseClass
 {
 	RTTI_CLASS(RefObject, RTTIBaseClass)
 private:
-	Atomic<int32> mRefCount = 1;
-
+	struct RefCounter
+	{
+		Atomic<int32> mRefCount = 1;
+		Atomic<int32> mWeakRefCount = 1;
+	};
+	RefCounter* mRefCounter = nullptr;
+	
 	template<typename taRefClassType, typename... taRefClassArgs>
 	friend class Ref;
 };
+
+template<typename taType>
+class WeakRef;
 
 template<typename taType, typename... taArgs>
 class Ref
@@ -19,10 +27,20 @@ public:
 	{
 		static_assert(std::is_base_of_v<RefObject, taType>);
 		mPtr = new taType(std::forward<taArgs>(inArgs)...);
+		mPtr->mRefCounter = new RefObject::RefCounter();
 	}
 
 	Ref(const Ref& inOther)
 	{
+		gAssert(inOther->mRefCount > 0, "Ref object is already destroyed!");
+		mPtr = inOther.mPtr;
+		mPtr->mWeakRefCount->mRefCount++;
+	}
+
+	Ref(const WeakRef& inOther)
+	{
+		gAssert(inOther.GetTotalRefCountPtr)
+		gAssert(inPtr->mRefCount > 0, "Ref object is already destroyed!");
 		mPtr = inOther.mPtr;
 		mPtr->mRefCount++;
 	}
@@ -30,9 +48,15 @@ public:
 	~Ref()
 	{
 		gAssert(mPtr != nullptr, "The object was destroyed but a Ref was still held!");
-		mPtr->mRefCount--;
-		if (mPtr->mRefCount <= 0)
+
+		mPtr->mRefCounter->mRefCount--;
+		mPtr->mRefCounter->mWeakRefCount--;
+
+		if (mPtr->mRefCounter->mRefCount <= 0)
 		{
+			if(mPtr->mRefCounter->mWeakRefCount <= 0)
+				delete mPtr->mRefCounter;
+
 			delete mPtr;
 		}
 	}
@@ -48,4 +72,46 @@ public:
 
 private:
 	taType* mPtr = nullptr;
+
+private:
+};
+
+template<typename taType>
+class WeakRef
+{
+public:
+	WeakRef(const Ref<taType>& inRef)
+	{
+		mPtr = inRef.mPtr;
+		mPtr->(*mAllRefCount)++;
+	}
+
+	WeakRef(const WeakRef<taType>& inWeakRef)
+	{
+		mPtr = inWeakRef.mPtr;
+		mPtr->(*mTotalRefCount)++;
+	}
+
+	~WeakRef()
+	{
+		mPtr->(*mAllRefCount)--;
+		// If this was the last Ref and WeakRef referencing the ptr then we can destroy the all ref count
+		if (*mAllRefCount <= 0)
+		{
+			delete mAllRefCount;
+		}
+	}
+
+	Ref<taType>* Get() const 
+	{ 
+		if(*mTotalRefCount >= 0)
+			return Ref<taType>(*this); 
+		return nullptr;
+	}
+
+	Atomic<int32>* GetTotalRefCountPtr() const { return mAllRefCount; }
+private:
+	taType* mPtr = nullptr;
+	RefObject::RefCounter* a = nullptr;
+	Atomic<int32>* mTotalRefCount = nullptr;
 };
