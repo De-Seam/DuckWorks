236640d5dd600509e54d67943d73787c201cc314
Sam van der Hoeven <hoevens@yahoo.com>
2024-04-23 19:36:16 +0200

    Added projectile header

---
+++ DuckWorks/DuckWorks/include/Game/Entity/Projectiles/Projectile.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Core/CoreBase.h"
+
+#include "Engine/Entity/CollisionActor.h"
+
+/*
+Projectile provides base functionality for all projectiles
+*/
+class Projectile : public CollisionActor
+{
+	RTTI_CLASS(Projectile, CollisionActor)
+public:
+	virtual void Update(float inDeltaTime) override;
+
+	void SetVelocity(fm::vec2 inVelocity) { mVelocity = inVelocity; }
+
+private:
+	fm::vec2 mVelocity;
+};
