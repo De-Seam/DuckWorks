c1ec885e0b7fa081bdeb84d6789c0367a2611cf7
Sam van der Hoeven <hoevens@yahoo.com>
2024-04-23 19:35:56 +0200

    Applied style guide

--- DuckWorks/DuckWorks/include/Core/Utilities/RTTI.h
+++ DuckWorks/DuckWorks/include/Core/Utilities/RTTI.h
@@ -8,39 +8,52 @@
 #define RTTI_CLASS(inClassName, inParentClassName) \
 public: \
 	using Base = inParentClassName; \
+\
 	static const char* sGetClassName() { return #inClassName; } \
+\
 	static const char* sGetParentClassName() { return #inParentClassName; } \
+\
 	virtual const char* GetClassName() const override { return inClassName::sGetClassName(); } \
+\
 	virtual const char* GetParentClassName() const override { return inClassName::sGetParentClassName(); } \
+\
 	virtual Json Serialize() override; \
+\
 	virtual void Deserialize(const Json& inJson) override; \
-	static const UID& sGetRTTIUID() { return s##inClassNameRTTIUID; } \
-	virtual const UID& GetRTTIUID() const override { return s##inClassNameRTTIUID; } \
+\
+	static const UID &sGetRTTIUID() { return s##inClassName##RTTIUID; } \
+\
+	virtual const UID &GetRTTIUID() const override { return s##inClassName##RTTIUID; } \
+\
 	virtual bool IsAUID(const UID& inRTTIUID) const override \
-		{ \
-			if (inRTTIUID == sGetRTTIUID()) \
-				return true; \
-			else \
-				return Base::IsAUID(inRTTIUID); \
-		} \
-template<typename taType> \
-static bool sIsA() \
-{ \
-	return sIsAUID(taType::sGetRTTIUID()); \
-} \
-static bool sIsAUID(const UID& inRTTIUID) \
-{ \
-if(inRTTIUID == sGetRTTIUID()) \
-	return true; \
-else \
-	return Base::sIsAUID(inRTTIUID); \
-} \
-private: \
-	static UID s##inClassNameRTTIUID; \
-public:
+	{ \
+		if (inRTTIUID == sGetRTTIUID()) \
+			return true; \
+		else \
+			return Base::IsAUID(inRTTIUID); \
+	} \
+\
+	template<typename taType> \
+	static bool sIsA() \
+	{ \
+		return sIsAUID(taType::sGetRTTIUID()); \
+	} \
+\
+	static bool sIsAUID(const UID& inRTTIUID) \
+	{ \
+	if(inRTTIUID == sGetRTTIUID()) \
+		return true; \
+	else \
+		return Base::sIsAUID(inRTTIUID); \
+	} \
+\
+	private: \
+		static UID s##inClassName##RTTIUID; \
+\
+	public:
 
 #define RTTI_CLASS_DEFINITION(inClassName) \
-	UID inClassName::s##inClassNameRTTIUID;
+	UID inClassName::s##inClassName##RTTIUID;
 
 #define RTTI_EMPTY_SERIALIZE_DEFINITION(inClassName) \
 	Json inClassName::Serialize() { return Base::Serialize(); } \
