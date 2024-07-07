75a4936443f7b267905397f839806fd397632654
Sam van der Hoeven <hoevens@yahoo.com>
2024-07-07 13:11:11 +0200

    Update DebugUIWindowEntityDetails.h

--- DuckWorks/DuckWorks/include/Engine/Debug/Windows/DebugUIWindowEntityDetails.h
+++ DuckWorks/DuckWorks/include/Engine/Debug/Windows/DebugUIWindowEntityDetails.h
@@ -1,9 +1,6 @@
 #pragma once
 #include "Engine/Debug/Windows/DebugUIWindow.h"
 
-class Entity;
-class ComponentBase;
-
 class DebugUIWindowEntityDetails : public DebugUIWindow
 {
 	RTTI_CLASS(DebugUIWindowEntityDetails, DebugUIWindow, StandardAllocator)
@@ -11,14 +8,9 @@ class DebugUIWindowEntityDetails : public DebugUIWindow
 public:
 	struct ConstructParameters : public Base::ConstructParameters {};
 
-	using Base::Base;
-
 	DebugUIWindowEntityDetails(const ConstructParameters& inConstructParameters = {}) : Base(inConstructParameters) {}
 
 	virtual void Update(float inDeltaTime) override;
-
-private:
-	WeakPtr<Entity> mSelectedEntity;
 
 private:
 };
