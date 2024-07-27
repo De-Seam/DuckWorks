943b794e89831c0bec7fb137bc402f0cfaec9a6b
Sam van der Hoeven <hoevens@yahoo.com>
2024-07-27 10:23:15 +0200

    Improved gSwap

--- DuckWorks/DuckCore/include/Core/Utilities/Utilities.h
+++ DuckWorks/DuckCore/include/Core/Utilities/Utilities.h
@@ -1,4 +1,10 @@
 #pragma once
+// Core includes
+#include <Core/Utilities/TypeID.h>
+
+// Std includes
+#include <cstring>
+
 template<typename taType>
 taType gMin(taType inA, taType inB)
 {
@@ -20,7 +26,8 @@ taType gClamp(taType inX, taType inMin, taType inMax)
 template<typename taType>
 void gSwap(taType& ioA, taType& ioB)
 {
-	taType temp = ioA;
-	ioA = ioB;
-	ioB = temp;
+	uint8 temp[sizeof(taType)];
+	memcpy(temp, &ioA, sizeof(taType));
+	memcpy(&ioA, &ioB, sizeof(taType));
+	memcpy(&ioB, &temp, sizeof(taType));
 }
