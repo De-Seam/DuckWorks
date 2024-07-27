248cc2f323ede391f3888325b302a0f47ada8087
Sam van der Hoeven <hoevens@yahoo.com>
2024-07-27 15:14:09 +0200

    Update EventMessages.h

--- DuckWorks/DuckEngine/include/Engine/Renderer/WindowEvents/EventMessages.h
+++ DuckWorks/DuckEngine/include/Engine/Renderer/WindowEvents/EventMessages.h
@@ -10,7 +10,6 @@ struct MsgWindowClosed : public MsgBase
 struct MsgWindowResized : public MsgBase
 {
 	RTTI_MSG()
-
 	uint32 mWidth = 0, mHeight = 0;
 };
 
@@ -27,14 +26,12 @@ struct MsgWindowGainedFocus : public MsgBase
 struct MsgWindowTextEntered : public MsgBase
 {
 	RTTI_MSG()
-
 	uint32 mUnicodeCharacter = 0;
 };
 
 struct MsgWindowKeyPressed : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Keyboard::Key mKey = sf::Keyboard::Unknown;
 	bool mAltDown = false;
 	bool mControlDown = false;
@@ -45,7 +42,6 @@ struct MsgWindowKeyPressed : public MsgBase
 struct MsgWindowKeyReleased : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Keyboard::Key mKey = sf::Keyboard::Unknown;
 	bool mAltDown = false;
 	bool mControlDown = false;
@@ -56,7 +52,6 @@ struct MsgWindowKeyReleased : public MsgBase
 struct MsgWindowMouseWheelMoved : public MsgBase
 {
 	RTTI_MSG()
-
 	int32 mDelta = 0; ///< Number of ticks the wheel has moved (positive is up, negative is down)
     int32 mX = 0; ///< X position of the mouse pointer, relative to the left of the owner window
     int32 mY = 0; ///< Y position of the mouse pointer, relative to the top of the owner window
@@ -65,7 +60,6 @@ struct MsgWindowMouseWheelMoved : public MsgBase
 struct MsgWindowMouseWheelScrolled : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Mouse::Wheel mWheel = sf::Mouse::VerticalWheel; ///< Which wheel (for mice with multiple ones)
 	float mDelta = 0; ///< Number of ticks the wheel has moved (positive is up, negative is down)
     int32 mX = 0; ///< X position of the mouse pointer, relative to the left of the owner window
@@ -75,21 +69,18 @@ struct MsgWindowMouseWheelScrolled : public MsgBase
 struct MsgWindowMouseButtonPressed : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::MouseButtonEvent mButtonEvent;
 };
 
 struct MsgWindowMouseButtonReleased : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::MouseButtonEvent mButtonEvent;
 };
 
 struct MsgWindowMouseMoved : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::MouseMoveEvent mMouseMoveEvent;
 };
 
@@ -106,62 +97,53 @@ struct MsgWindowMouseLeft : public MsgBase
 struct MsgWindowJoystickButtonPressed : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::JoystickButtonEvent mJoystickButtonEvent;
 };
 
 struct MsgWindowJoystickButtonReleased : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::JoystickButtonEvent mJoystickButtonEvent;
 };
 
 struct MsgWindowJoystickMoved : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::JoystickMoveEvent mJoystickMoveEvent;
 };
 
 struct MsgWindowJoystickConnected : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::JoystickConnectEvent mJoystickConnectEvent;
 };
 
 struct MsgWindowJoystickDisconnected : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::JoystickConnectEvent mJoystickConnectEvent;
 };
 
 struct MsgWindowTouchBegan : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::TouchEvent mTouchEvent;
 };
 
 struct MsgWindowTouchMoved : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::TouchEvent mTouchEvent;
 };
 
 struct MsgWindowTouchEnded : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::TouchEvent mTouchEvent;
 };
 
 struct MsgWindowSensorChanged : public MsgBase
 {
 	RTTI_MSG()
-
 	sf::Event::SensorEvent mSensorEvent;
 };
