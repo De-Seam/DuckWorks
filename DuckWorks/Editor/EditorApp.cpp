13115952300ee8b162b1ece23cfb5b209adb91eb
Sam van der Hoeven <hoevens@yahoo.com>
2024-12-23 18:07:14 +0100

    Added rendering game to render target

--- DuckWorks/Editor/EditorApp.cpp
+++ DuckWorks/Editor/EditorApp.cpp
@@ -1,11 +1,21 @@
+#include <DuckCore/Math/Transform.h>
 #include <Editor/EditorApp.h>
 
 EditorApp::EditorApp()
 {
+	mGameRenderTarget = gEngine->GetManager<Renderer>().CreateTexture(DC::IVec2(800, 600));
+
 	mGameApp = DC::gMakeUnique<GameApp>();
 }
 
 void EditorApp::Update(float inDeltaTime)
 {
-	mGameApp->Update(inDeltaTime);
+	{
+		// Render the game to the game render target
+		Renderer::ScopedRenderTarget scoped_render_target(mGameRenderTarget);
+		mGameApp->Update(inDeltaTime);
+	}
+	DC::Transform2D transform;
+	transform.mHalfSize = DC::FVec2(800, 600) / 2.0f;
+	gEngine->GetManager<Renderer>().DrawTexture(mGameRenderTarget, transform);
 }
