30f41b506e96c28dfddd00b11918b745a1190f9e
Sam van der Hoeven <hoevens@yahoo.com>
2025-01-03 14:17:12 +0100

    Update GameplayComponents.h

--- DuckWorks/Game/Entity/Components/GameplayComponents.h
+++ DuckWorks/Game/Entity/Components/GameplayComponents.h
@@ -17,3 +17,16 @@ struct PlayerMovementComponent
 	float mMaxVelocity = 100.0f;
 	float mAcceleration = 80.0f;
 };
+
+struct BlockingComponent
+{
+	enum class BlockingType : uint8
+	{
+		None, // None. Other entities may walk over this.
+		Full, // Full. Other entities may not walk over this.
+		Half, // Half. Movement speed is lowered while "climbing" over this.
+		Pawn // Pawn. Other entities may walk over this, but not stand on it.
+	};
+
+	BlockingType mBlockingType = BlockingType::None;
+};
